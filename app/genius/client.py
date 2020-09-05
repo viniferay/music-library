12b03dc9c049bfca64c1f06b5ab6b33ae7ddb6b7
Vinicius Ferreira <vferreira@mastertech.com.br>
2020-09-05 14:21:07 -0300

    Create client to Genius API

---
+++ app/genius/client.py
@@ -0,0 +1,14 @@
+import os
+
+import requests
+
+
+TOKEN = 'Bearer ' + os.environ.get('GENIUS_OUTH2')
+BASE_URL = 'http://api.genius.com/'
+
+
+def get_artist_songs(artist_id):
+    url = f'{BASE_URL}artists/{artist_id}/songs?sort=popularity&per_page=10'
+    response = requests.get(url=url, headers={'Authorization': TOKEN})
+
+    return response
