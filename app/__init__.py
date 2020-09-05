b3e297110fdf7dd152020edda1fd81ddc853c3a6
Vinicius Ferreira <vferreira@mastertech.com.br>
2020-09-05 14:29:20 -0300

    Create artist module

--- app/__init__.py
+++ app/__init__.py
@@ -7,4 +7,7 @@ def create_app(config_name='development'):
     from app.genius import bp as genius_client_bp
     app.register_blueprint(genius_client_bp)
 
+    from app.artist import bp as artist_bp
+    app.register_blueprint(artist_bp)
+
     return app
---
+++ app/artist/__init__.py
@@ -0,0 +1,3 @@
+from flask import Blueprint
+
+bp = Blueprint('artist', __name__)
