8598de146658f5eeb77afc0b715c174dae97dd3c
Vinicius Ferreira <vferreira@mastertech.com.br>
2020-09-05 13:15:40 -0300

    Create genius  module

--- app/__init__.py
+++ app/__init__.py
@@ -4,4 +4,7 @@ from flask import Flask
 def create_app(config_name='development'):
     app = Flask(__name__)
 
+    from app.genius import bp as genius_client_bp
+    app.register_blueprint(genius_client_bp)
+
     return app
