c55a33650e64ca19e862f61d1e520d0652b02eb3
Vinicius Ferreira <vferreira@mastertech.com.br>
2020-09-05 17:03:45 -0300

    Create schema to songs camelcase default json

--- app/artist/schemas.py
+++ app/artist/schemas.py
@@ -5,3 +5,14 @@ class PrimaryArtistSchema(Schema):
     id = fields.Integer()
     name = fields.String()
     url = fields.URL()
+
+
+class SongSchema(Schema):
+    id = fields.Integer()
+    full_title = fields.String(data_key='fullTitle')
+    title = fields.String()
+    primary_artist = fields.Nested(PrimaryArtistSchema, data_key='primaryArtist')
+
+
+class SongsSchema(Schema):
+    songs = fields.List(fields.Nested(SongSchema), many=True)
