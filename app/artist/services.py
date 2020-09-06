d54c3d23f98d3918741c156882ec6d57f9f3755c
Vinicius Ferreira <vferreira@mastertech.com.br>
2020-09-06 16:37:50 -0300

    Implement put_data into service to save artist name

--- app/artist/services.py
+++ app/artist/services.py
@@ -1,7 +1,12 @@
-from app.genius.client import get_artist_songs
+from app import dynamoDB
+from app.genius.client import get_artist_songs, get_artist_data
 
 
 def get_songs_by_artist_id(artist_id: int) -> dict:
+    artist_data = get_artist_data(artist_id)
+    artist_name = artist_data.json()['response']['artist']['name']
+    dynamoDB.put_data(artist_name)
+
     genius_response = get_artist_songs(artist_id)
     response_json = genius_response.json()['response']
 
