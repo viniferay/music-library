8872e0974e25f5d28a1a741f2b696111a65d1f39
Vinicius Ferreira <vferreira@mastertech.com.br>
2020-09-07 13:22:01 -0300

    Implement cache app

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,20 @@
 from flask import Flask
-from flask_redis import FlaskRedis
+from flask_caching import Cache
 
 from app.dynamoDB.client import DynamoDBClient
 from config import Config
 
 dynamoDB = DynamoDBClient()
-redis_client = FlaskRedis()
+cache = Cache(config={'CACHE_TYPE': 'redis'})
 
 
 def create_app(config_name='development'):
     app = Flask(__name__)
     app.config.from_object(Config)
 
+    cache.init_app(app)
     dynamoDB.init_client(app)
-    redis_client.init_app(app)
-    
+
     from app.genius import bp as genius_client_bp
     app.register_blueprint(genius_client_bp)
 
