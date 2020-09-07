229a6316e8b55264ed1f9d442a46981933cb70a9
Vinicius Ferreira <vferreira@mastertech.com.br>
2020-09-07 13:19:18 -0300

    Add cache config

--- config.py
+++ config.py
@@ -4,3 +4,6 @@ import os
 class Config:
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    CACHE_DEFAULT_TIMEOUT = os.environ.get('CACHE_DEFAULT_TIMEOUT', 86000)
+    CACHE_REDIS_PASSWORD = os.environ.get('CACHE_REDIS_PASSWORD', 'music-library!')
+    CACHE_REDIS_PORT = os.environ.get('CACHE_REDIS_PORT', 6379)
