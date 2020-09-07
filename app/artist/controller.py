1156afac61f6318a4567d46b2261f8aeb1842455
Vinicius Ferreira <vferreira@mastertech.com.br>
2020-09-07 13:21:36 -0300

    Add the cache lookup in the controler

--- app/artist/controller.py
+++ app/artist/controller.py
@@ -1,4 +1,4 @@
-from flask import jsonify
+from flask import jsonify, request
 
 from app.artist import bp
 from app.artist.schemas import SongsSchema
@@ -8,6 +8,8 @@ from app.artist.services import get_songs_by_artist_id
 @bp.route('/artists/<int:artist_id>/songs', methods=['GET'])
 def get_songs_from_artist(artist_id):
     schema = SongsSchema()
-    songs = get_songs_by_artist_id(artist_id)
+
+    caching = request.args.get('cache', True)
+    songs = get_songs_by_artist_id(artist_id, caching)
 
     return jsonify(schema.dump(songs)), 200
